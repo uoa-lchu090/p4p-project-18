6bcc4efd1156d322b085810c4a93387d839b4f1e
rando <randomguy7373@live.com>
2022-06-03 14:53:56 +1200

    added error handling and counter to file_sorter

--- file_sorter.py
+++ file_sorter.py
@@ -6,10 +6,15 @@ df = pd.read_excel(path)
 
 
 def sort(input_folder, output_folder):
+    notfound = 0
 
     for i in range(len(df)):
         file=df.get("Column1").tolist()[0]
-        shutil.copy(input_folder+"//"+file, output_folder+"//"+file)
+        try:
+            shutil.copy(input_folder+"//"+file, output_folder+"//"+file)
+        except FileNotFoundError:
+            notfound+=1
 
+    return notfound
 
-sort(".//clips", ".//clips2")
+print(sort(".//clips", ".//clips2"))
