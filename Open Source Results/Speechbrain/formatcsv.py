6ef5b31b73735e2531fb67f43dd5ab4cd62f53a4
rando <randomguy7373@live.com>
2022-08-05 13:16:38 +1200

    updated formatter to work with lists

--- Open Source Results/Speechbrain/formatcsv.py
+++ Open Source Results/Speechbrain/formatcsv.py
@@ -1,23 +1,26 @@
 import csv
 
-def format(filename):
+def format(filelist):
 
     fields = ['id', 'filename', 'text']
     rows = []
+    id = 0
 
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        id = 0
-        for row in csv_reader:
-            row.insert(0, id)
-            id += 1
-            rows.append(row)
 
-    with open('new' + filename, 'w') as new_file:
+    for filename in filelist:
+        with open(filename, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+
+            for row in csv_reader:
+                row.insert(0, id)
+                id += 1
+                rows.append(row)
+
+    with open('data.csv', 'w') as new_file:
 
         csv_writer = csv.writer(new_file)
         csv_writer.writerow(fields)
         csv_writer.writerows(rows)
 
 if __name__ == "__main__":
-    format('JL_output_speechbrain.csv')
+    format(['JL_output_speechbrain.csv'])
