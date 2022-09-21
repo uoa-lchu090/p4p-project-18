42aa54f40ebafb884759e5ef6729b860d75cbb67
uoa-lchu090 <lchu090@aucklanduni.ac.nz>
2022-09-22 11:29:18 +1200

    More reformatting
    added preprocessing on MZ files

--- dataset preprocessing/file_sorter.py
+++ dataset preprocessing/file_sorter.py
@@ -1,15 +1,16 @@
 import shutil
 
 import pandas as pd
-path = ".\WER\mansfield_excel.xlsx"
-df = pd.read_excel(path)
+path = "./similar_word.xlsx"
+df = pd.read_excel(path, sheet_name="NZoutput")
 
 
 def sort(input_folder, output_folder):
     notfound = 0
 
     for i in range(len(df)):
-        file=df.get("Column1").tolist()[i]
+        file=df.get("file").tolist()[i]
+        print(i)
         try:
             shutil.copy(input_folder+"//"+file, output_folder+"//"+file)
         except FileNotFoundError:
@@ -17,4 +18,9 @@ def sort(input_folder, output_folder):
 
     return notfound
 
-print(sort(".//clips", ".//clips2"))
+
+if __name__ == '__main__':
+    notfound = sort("./NZ", "./NZsimilar")
+    print("done")
+    print(notfound)
+    #print(sort(".//clips", ".//clips2"))
